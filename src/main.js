48cf044628ae2c3821103128226b4544105fde5f
Lyric <5h3ll3x@gmail.com>
2023-02-18 17:04:54 +0900

    update version

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 
-const version = "2.0.3";
+const version = "2.0.4";
 
 export default function (opts) {
   const styleUrl = `https://cdn.jsdelivr.net/npm/@foxone/talkee@${version}/dist/style.css`;
   const scriptUrls = [
     { name: "vue", url: `https://cdn.jsdelivr.net/npm/vue@3.2.45/dist/vue.global.prod.js` },
-    { name: "mvm", url: `https://cdn.jsdelivr.net/npm/@foxone/mvm@0.1.27/dist/mvm.min.js` },
+    { name: "mvm", url: `https://cdn.jsdelivr.net/npm/@foxone/mvm@0.1.29/dist/mvm.min.js` },
     { name: "talkee", url: `https://cdn.jsdelivr.net/npm/@foxone/talkee@${version}/dist/index.umd.js` },
   ];
 
